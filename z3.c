bbf48e945d0a4854b6cfed45e502d525289d9912
Shruti Holkar <138362092+Shrutiholkar@users.noreply.github.com>
2023-07-20 20:30:26 +0530

    Add files via upload

---
+++ z3.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+
+struct stud
+{
+int rno;
+char name[20];
+float per;
+char sclass[20];
+}s1[100];
+
+int main()
+{
+int i,n,rn;
+
+printf("enter limit");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("enter rno name per sclass");
+scanf("%d%s%f%s",&s1[i].rno,s1[i].name,&s1[i].per,s1[i].sclass);
+}
+
+printf("enter  roll no to search");
+scanf("%d",&rn);
+for(i=0;i<n;i++)
+{
+if(s1[i].rno==rn)
+{
+printf("\n roll no=%d",s1[i].rno);
+printf("\n name=%s",s1[i].name);
+printf("\n per=%f ",s1[i].per);
+
+}
+
+
+}
+}
+
+
+
